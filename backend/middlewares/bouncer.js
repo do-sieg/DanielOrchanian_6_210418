90cc6d8ad6305cc6e42a5021aea439ca585390aa
do-sieg <saleth.admin@gmail.com>
2021-05-05 21:40:06 +0200

    Updated bouncer response

--- backend/middlewares/bouncer.js
+++ backend/middlewares/bouncer.js
@@ -5,5 +5,5 @@ const MAX_WAIT_TIME_SEC = 24 * 60 * 60;
 export const bouncer = require("express-bouncer")(MIN_WAIT_TIME_SEC * 1000, MAX_WAIT_TIME_SEC * 1000);
 
 bouncer.blocked = function (req, res, next, remaining) {
-    res.send(429, "Too many requests have been made, please wait " + remaining / 1000 + " seconds");
+    res.status(429).send({ message: "Too many requests have been made, please wait " + remaining / 1000 + " seconds" });
 };
